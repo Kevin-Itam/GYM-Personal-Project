dc0c3f999df98ba707f0330bce385eb8b84fbabc
Your Name <kevinitamar16@gmail.com>
2023-05-29 21:52:32 -0300

    rrtttr

--- js/javaScript.js
+++ js/javaScript.js
@@ -96,23 +96,21 @@ jQuery(document).ready(function () {
 });
 
 //Valida compo Telefone
-function telefone_validation(telefone , telefonel) {
+function telefone_validation(telefone) {
   //retira todos os caracteres menos os numeros
   telefone = telefone.value.replace(/\D/g, '');
   
-  telefonel = telefonel.value.replace(/\D/g, '');
-
   //verifica se tem a qtde de numero correto
-  if (!(telefone.length && telefonel.length >= 10 && telefone.length && telefonel.length <= 11)) return false;
+  if (!(telefone.length>= 10 && telefone.length <= 11)) return false;
 
   //Se tiver 11 caracteres, verificar se começa com 9 o celular
-  if (telefone.length && telefonel.length == 11 && parseInt(telefone.substring(2, 3)&& telefonel.substring(2,3)) != 9) return false;
+  if (telefone.length == 11 && parseInt(telefone.substring(2, 3)) != 9) return false;
 
   //verifica se não é nenhum numero digitado errado (propositalmente)
   for (var n = 0; n < 10; n++) {
 
     if (telefone == new Array(11).join(n) || telefone == new Array(12).join(n)) return false;
-    if (telefonel == new Array(11).join(n) || telefonel == new Array(12).join(n)) return false;
+   
   }
   //DDDs validos
   var codigosDDD = [11, 12, 13, 14, 15, 16, 17, 18, 19,
@@ -129,7 +127,41 @@ function telefone_validation(telefone , telefonel) {
   }
 
 
-  if (telefone.length == 10 && telefonel.length == 10 && [2, 3, 4, 5, 7].indexOf(parseInt(telefone.substring(2, 3) && telefonel.substring(2,3))) == -1) {
+  if (telefone.length == 10 && [2, 3, 4, 5, 7].indexOf(parseInt(telefone.substring(2, 3))) == -1) {
+    return false;
+  }
+
+
+  return true;
+
+}
+
+function telefone_validation(telefonel) {
+  //retira todos os caracteres menos os numeros
+  telefonel = telefonel.value.replace(/\D/g, '');
+
+  //verifica se tem a qtde de numero correto
+  if (!(telefonel.length >= 10 && telefonel.length <= 11)) return false;
+
+  //Se tiver 11 caracteres, verificar se começa com 9 o celular
+  if (telefonel.length == 11 && parseInt(telefonel.substring(2,3)) != 9) return false;
+
+  //verifica se não é nenhum numero digitado errado (propositalmente)
+  for (var n = 0; n < 10; n++) {
+
+    if (telefonel == new Array(11).join(n) || telefonel == new Array(12).join(n)) return false;
+  }
+  //DDDs validos
+  var codigosDDD = [11, 12, 13, 14, 15, 16, 17, 18, 19,
+    21, 22, 24, 27, 28, 31, 32, 33, 34,
+    35, 37, 38, 41, 42, 43, 44, 45, 46,
+    47, 48, 49, 51, 53, 54, 55, 61, 62,
+    64, 63, 65, 66, 67, 68, 69, 71, 73,
+    74, 75, 77, 79, 81, 82, 83, 84, 85,
+    86, 87, 88, 89, 91, 92, 93, 94, 95,
+    96, 97, 98, 99];
+
+  if (telefonel.length == 10 && [2, 3, 4, 5, 7].indexOf(parseInt(telefone.substring(2, 3) && telefonel.substring(2,3))) == -1) {
     return false;
   }
 
