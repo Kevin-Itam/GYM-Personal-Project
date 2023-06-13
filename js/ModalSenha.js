6bbb9b542405ed5b14eb5bda4334b1e4c71cfbd9
Your Name <kevinitamar16@gmail.com>
2023-06-12 22:08:58 -0300

    segunda 12/06/23

--- js/ModalSenha.js
+++ js/ModalSenha.js
@@ -1,51 +1,46 @@
 /*FUNÇÃO ALTERAR SENHA */
 function abrirSenha(abrir) {
- 
-        let modal = document.getElementById(abrir);
 
-        modal.style.display = 'flex'; 
+    let modal = document.getElementById(abrir);
+
+    modal.style.display = 'flex';
 
 }
 
 
-function fecharSenha(abrir){
+function fecharSenha(abrir) {
 
     let modal = document.getElementById(abrir);
 
     modal.style.display = 'none';
-    
+
 }
 
-function abrir(id_registro){
-    let dc = document.querySelector('.backgroundModal[id="'+ id_registro +'"]')
+function abrir(id_registro) {
+    let dc = document.querySelector('.backgroundModal[id="' + id_registro + '"]')
     dc.style.display = 'block';
-    
+
 }
 
-function fechar(id_fechar){
-    
-    let modal = document.querySelector('.backgroundModal[id="'+ id_fechar +'"]')
+function fechar(id_fechar) {
+
+    let modal = document.querySelector('.backgroundModal[id="' + id_fechar + '"]')
 
     modal.style.display = 'none';
 }
 /*============ tabelas ============= */
 
-const cad = document.querySelector("#cad");
-const plan = document.querySelector("#plan");
 
-
-function abrir_cad(abr_cad){
-    let modal = document.getElementById(abr_cad);
-    modal.style.display = 'block'; 
-
-    // var tab = plan;
-    // tab.style.display = 'none';
+function abrir_cad(abr_cad) {
+    let cadastro = document.getElementById(abr_cad);
+    var plan = document.getElementById('plan');
+    cadastro.style.display = 'block';
+    plan.style.display = 'none';
 }
 
-function abrir_plan(abr_plan){
-    let modal = document.getElementById(abr_plan);
-    modal.style.display = 'block'; 
-    // var tab = cad;
-    // tab.style.display = 'none';
-
+function abrir_plan(abr_plan) {
+    let plano = document.getElementById(abr_plan);
+    var cad = document.getElementById('cad');
+    plano.style.display = 'block';
+    cad.style.display = 'none';
 }
--- pages/pg_plano_user.php
+++ pages/pg_plano_user.php
@@ -53,7 +53,7 @@ $result = $conn->query($sql);
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="../css/style_lateral.css" rel="stylesheet" type="text/css">
-    <script src="https://kit.fontawesome.com/3a1453d3f1.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <title>Página do Administador</title>
@@ -90,6 +90,27 @@ $result = $conn->query($sql);
     .login-wrap {
         background: white;
         border-radius: 5px;
+    }
+
+    .pg {
+        line-height: 60px;
+        font-weight: 600;
+        white-space: nowrap;
+        padding: 10px;
+        margin: 10px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 100%;
+        color: #000000;
+    }
+
+    .tabela .dv-title {
+        max-width: 100px;
+        display: flex;
+        flex-direction: row;
+        position: relative;
+        z
     }
 </style>
 <script>
@@ -152,7 +173,7 @@ $result = $conn->query($sql);
             </form>
         </div>
     </div>
-
+    <!-- <i class="fa-solid fa-house"></i> -->
     <!--============ HEADER ===========-->
     <header>
         <div class="nav-top">
@@ -168,8 +189,10 @@ $result = $conn->query($sql);
                 <div class="container-fluid">
                     <form class="d-flex" method="GET" action="">
                         <div class="input-group">
-                            <input type="search" name="nome" class="form-control rounded" placeholder="Pesquisar" aria-label="Search" aria-describedby="search-addon" value="<?php if (isset($_GET['nome_aluno']))
-                                    echo $_GET['nome_aluno']; ?>" style="color:black!important;border:2px solid black;"/>
+                            <input type="search" name="nome" class="form-control rounded" placeholder="Pesquisar"
+                                aria-label="Search" aria-describedby="search-addon" value="<?php if (isset($_GET['nome_aluno']))
+                                    echo $_GET['nome_aluno']; ?>"
+                                style="color:black!important;border:2px solid black;" />
                             <button type="submit" class="btn-src">Pesquisar</button>
                         </div>
                     </form>
@@ -189,35 +212,33 @@ $result = $conn->query($sql);
     <!--========================== SIDEBAR ============================-->
     <div class="forum_menu">
         <ul>
-            <li class="list active fa-house">
+            <li class="list active">
                 <a class="alink">
                     <span class="titulo-menu">Menu</span>
-                    <span><i class="fa-solid fa-house"></i></span>
+                </a>
+            </li>
 
-                    <span class="titulo"><a class="a" href="../index_Apre.php">Página principal</a></span>
-                </a>
+            <li class="list active " style="display: flex;flex-direction: column;row-gap: 2vh;">
+                <a class="alink" href="../index_Apre.php"><span class="titulo"
+                        style="cursor:pointer;background-color: white;"> Voltar ao menu </span></a>
             </li>
-            <li class="list active fa-house">
-                <a class="alink">
-                    <a href="../img/registrado.png"></a>
-                    <span class="titulo"><a class="a" onclick="abrir_cad('cad')">Visualizar cadastros</a></span>
-                </a>
+
+            <li class="list active " style="display: flex;flex-direction: column;row-gap: 2vh;"><a class="alink"
+                    onclick="abrir_cad('cad')"><span class="titulo" style="cursor:pointer;background-color: white;">
+                        Visualizar cadastros </span></a>
             </li>
-            <li class="list active fa-house">
-                <a class="alink">
-                    <a href="../img/registrado.png"></a>
-                    <span class="titulo"><a class="a" onclick="abrir_plan('plan')">Visualizar cadastros</a></span>
-                </a>
+            <li class="list active " style="display: flex;flex-direction: column;row-gap: 2vh;">
+                <a class="alink" onclick="abrir_plan('plan')"><span class="titulo"
+                        style="cursor:pointer;background-color: white;"> Visualizar Plano </span></a>
             </li>
         </ul>
     </div>
-
     <!--========================== TABELA  Cadastros ============================-->
     <!--=========================================================================-->
 
 
     <div class="tabela" id="cad">
-        <a>Cadastros</a>
+        <p style="margin-top:20px;margin-bottom:20px;font-size:20px;font-weight:700;">Cadastros</p>
         <table class="table">
             <thead class="thead-light">
                 <tr>
@@ -279,8 +300,12 @@ $result = $conn->query($sql);
     <!--============================================================================-->
     <!--==================================== TABELA  PLANOS ========================-->
 
-    <div class="tabela" id="plan">
-        <a>Planos</a>
+    <div class="tabela " id="plan">
+        <p style="margin-top:20px;margin-bottom:10px;font-size:20px;font-weight:700;">Planos</p>
+        <div class="dv-title">
+            <button onclick="abrirSenha('abrir')" class="btn btn-success"
+                style="height: 40px;margin-bottom:20px">Cadastrar</buttom>
+        </div>
         <table class="table">
             <thead class="thead-light">
                 <tr>
@@ -290,11 +315,6 @@ $result = $conn->query($sql);
                     <th scope="col">Excluir</th>
                     <th scope="col">Editar</th>
                 </tr>
-                <div>
-                    <button onclick="abrirSenha('abrir')" class="btn btn-success" style="height: 40px; width:100px ;">
-                        <p>Cadastrar</p>
-                        </buttom>
-                </div>
             </thead>
             <tbody>
                 <?php
@@ -309,31 +329,42 @@ $result = $conn->query($sql);
                     echo "<td>" . "<button class='btn btn-success' onclick='abrir(" . $plano['idl_planos'] . ")' >Editar</button>" . "</td>";
                     echo '</tr>';
 
-                    echo " <div class='backgroundModal' id=" . $plano["idl_planos"] . ">
-                    <div class='login-wrap p-4 p-md-5'>
-                        <span class='X-lateral-vei' onclick='fechar(" . $plano['idl_planos'] . ") '><i class='fa-regular fa-circle-xmark'></i></span>
-                        <h3 class='text-center mb-4' style='padding-top: 50px; color: white; font-size: 25px; color:#0e0e0e;'>Cadastrar o novo Plano</h3>
-                        <form action='../inc/editar_plano.php' method='post' class='login-form'>
-                            <input type='hidden' name='id_user' value=". $plano['idl_planos'] .">
-                            <div class='form-group'>
-                                <input name='nome_plano' type='text' class='form-control rounded-left' placeholder='Nome do Plano' required='' style='margin-top: 25px;' value=". $plano['nome_plano'] .">
-                            </div>
-                            <div class='form-group'>
-                                <input name='valor_plano' type='text' class='form-control rounded-left' placeholder='Valor do Plano' required='' style='margin-top: 25px;' value=". $plano['valor_plano'] .">
-                            </div>
-                            <div class='form-group'>
-                                <input name='opcao_plano' type='text' class='form-control rounded-left' placeholder='Mensal/Trimensal/Anual' required='' style='margin-top: 25px;' value=". $plano['opcao_plano'] .">
-                            </div>
-                            <div class='form-group'>
-                                <label for='exampleFormControlTextarea1'>Descrição do Plano</label>
-                                <textarea  name='desc_plano' class='form-control' id='exampleFormControlTextarea1' rows='3' >". $plano['desc_plano'] ."</textarea>
-                            </div>
-                            <div class='form-group'>
-                                <button type='submit' class='btn btn-outline-warning' style='width: 150px; margin-top: 25px;'>Salvar</button>
-                            </div>
-                        </form>
-                    </div>
-                </div>";
+                    echo " <div class='backgroundModal'style='position:absolute!important;top:0%;left:20%;transform:transition(-50%,-0%);' id=" . $plano["idl_planos"] . ">
+                                <div class='login-wrap p-4 p-md-5'>
+                                    <span class='X-lateral-vei' style='cursor:pointer;' onclick='fechar(" . $plano['idl_planos'] . ") '><i class='fa-regular fa-circle-xmark'></i></span>
+                                    <h3 class='text-center mb-4' style='padding-top: 50px; color: white; font-size: 25px; color:#0e0e0e;'>Cadastrar o novo Plano</h3>
+                                    <form action='../inc/editar_plano.php' method='post' class='login-form'>
+
+                                        <input type='hidden' name='id_user' value=" . $plano['idl_planos'] . ">
+                                        
+                                        <div class='form-group'>
+                                            <input name='nome_plano' type='text' class='form-control rounded-left' placeholder=' ' style='margin-top: 30px;' value=" . $plano['nome_plano'] . ">
+                                            <label for='firstname' class='placeholder'> Nome do Plano</label>
+                                        </div>
+
+                                        <div class='form-group'>
+                                            <input name='valor_plano' type='text' class='form-control rounded-left' placeholder=' ' style='margin-top: 30px;' value=" . $plano['valor_plano'] . ">
+                                            <label for='firstname' class='placeholder'> Valor do Plano</label>
+                                        </div>
+
+                                        <div class='form-group'>
+                                            <input name='opcao_plano' type='text' class='form-control rounded-left' placeholder=' ' style='margin-top: 30px;' value=" . $plano['opcao_plano'] . ">
+                                            <label for='firstname' class='placeholder'>Opçao de Planos</label>
+                                        </div>
+
+
+                                        <div class='form-outline mb-4'>
+                                            <label for='exampleFormControlTextarea1'style='position:relative;top:1vh;color:black;'>Adicione uma descrição ao plano</label>
+                                            <textarea  name='desc_plano' class='form-control' id='exampleFormControlTextarea1' rows='4' style='position:relative;top:1vh; resize:none;'>" . $plano['desc_plano'] . "</textarea>
+                                        </div>
+
+                                        <div class='form-group'>
+                                            <button type='submit' class='btn btn-outline-warning' style='width: 150px; margin-top:30px;background-color:black;color:white;position:relative;left:35%;'>Salvar</button>
+                                        </div>
+
+                                    </form>
+                                </div>
+                            </div>";
                 }
                 ?>
             </tbody>
@@ -342,9 +373,6 @@ $result = $conn->query($sql);
             </tbody>
         </table>
     </div>
-    <?php
-
-    ?>
 </body>
 
 </html>
